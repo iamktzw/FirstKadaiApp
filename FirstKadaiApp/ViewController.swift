db5ca562488c72282ecd2a424e6eaa1be640790d
kyoko.kitazawa <k.kitazawa.2019@gmail.com>
2020-08-16 15:04:06 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by チョコマックぷろ on 2020/08/16.
+//  Copyright © 2020 チョコマックぷろ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
